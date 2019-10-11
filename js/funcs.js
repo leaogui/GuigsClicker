650755bd9f2c9e05e362e4ba42ef8e7e5ab515bb
gghosth <42755106+gghosth@users.noreply.github.com>
2019-10-10 23:27:37 -0300

    tentando

--- js/funcs.js
+++ js/funcs.js
@@ -1,10 +1,29 @@
-const Jogador = require('player');
+class Jogador{
+
+    constructor(nome, idade){
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    getNome(){
+        return this.nome;
+    }
+
+    getIdade(){
+        return this.idade;
+    }
+
+    test(){
+        alert(this.getIdade());
+    }
+
+}
 
 function novoPlayer()
 {
-    
-    player = new Jogador(document.getElementById("nome"), document.getElementById("idade"));
 
-    test();
+    player = new Jogador(document.getElementById("nome").value, document.getElementById("idade").value);
+
+    player.test();
 
 }
